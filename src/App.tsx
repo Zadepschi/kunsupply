899da82a3fa942b44d63323c9e3a2384d026116c
Nati Zadepschi <zadepschi@yahoo.com>
2026-06-18 14:49:43 +0300

    Add React Vite project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import logo from './assets/logo.png'
+import './App.css'
+
+function App() {
+  return (
+    <section id="center">
+      <img src={logo} alt="Hero" />
+    </section>
+  )
+}
+
+export default App
